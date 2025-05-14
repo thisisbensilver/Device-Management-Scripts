8f55a6bb3778fe1c1967443b188280a00e5300bc
Ben Silver <7258795+thisisbensilver@users.noreply.github.com>
2025-05-14 15:24:49 -0400

    Update Remote Download And Install.sh

--- SentinelOne/Remote Download And Install.sh
+++ SentinelOne/Remote Download And Install.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 ###################################################################
 #
-#   Written by @thisisbensilver in May 2025
+#   Written by @thisisbensilver in May 2025 
 #
 ###################################################################
 #
 #   This script downloads a SentinelOne installer package
 #   from a remote URL and installs it from a temp directory.
 #
-#   Why might you need this?
 #   Your SentinelOne Token needs to be in the same directory
 #   as the installer, and not all MDM solutions let you do that.
-#   I'm looking at you, MaaS360!
-#
+# 
 #   So this lets you install SentinelOne via script instead-
 #   Quick and dirty!
-#
+#  
 #   You should push out any MobileConfig policies prior to this
 #   Otherwise users get Security & Privacy permission pop-ups
 #
@@ -24,6 +22,9 @@
 #   I don't know how you'd break anything...
 #   But don't get mad at me if you do!
 #
+###################################################################
+# Script name
+myScriptTitle="Install SentinelOne from Remote Host"
 ###################################################################
 
 
@@ -39,11 +40,10 @@
 #
 
 # URL to your installer package (download from SentinelOne portal)
-fileLoc="https://YOURSERVER"
+fileLoc="CHANGE_ME_TO_YOUR_DOWNLOAD_LINK"
 
 # Registration Token (copy from SentinelOne portal)
-regToken="CHANGE ME"
-
+regToken="CHANGE_ME_TO_YOUR_REGISTRATION_TOKEN"
 #
 ###################################################################
 
@@ -59,35 +59,28 @@ regToken="CHANGE ME"
 # You can leave these as-is, unless you wanna tinker!
 #
 
-# Script name
-myScriptTitle="Install SentinelOne from Remote Host"
-
 # Location of temp install directory
-# No final slash!
-myDir="/tmp/S1"
+myDir="/tmp/S1" #No final slash!
 
 # Name the LogFile "YYYY-MM-DD_HH-MM-AM"
 myTime=$(date +"%Y-%m-%d_%I-%M-%p")
 myLogFile="$myDir/$myTime.txt"
 
-# Custom function to log "[datestamp] result"
+#Let Echo display whitespace character
+set -e
+
+
+# Some custom logging functions for clean output
+# Output "[datestamp] result"
 log() {
     echo "[$(date '+%Y-%m-%d %I:%M:%S %p')] $*"
 }
 
-# Custom function to log "result"
+# Output the result with no datestampe
 logNoTime(){
     echo -e "$*"
 }
 
-# Custom function to log something in a fancy banner
-logFancy(){
-    echo "###################################################################"
-    echo ""
-    echo -e "$*"
-    echo ""
-    echo "###################################################################"
-}
 #
 ###################################################################
 
@@ -102,28 +95,35 @@ logFancy(){
 #       Run the Script       #
 #                            ######################################
 #
+# Do everything within the main() function
+# This lets us put custom functions at the bottom of the script
 
+    
 # Remove the temp Directory if there is anything there
-rm -r $myDir
+if [ -d "$myDir" ]; then
+    sudo rm -r "$myDir"
+fi
+
 
 # Create the Directory and Log file and begin the script
-mkdir -p $myDir
-touch $myLogFile
+mkdir -p "$myDir"
+touch "$myLogFile"
 exec > >(tee -a "$myLogFile") 2>&1
-logFancy "$myScriptTitle"
+logNoTime "$myScriptTitle"
 logNoTime ""
+
 
 # Check if SentinelOne is installed first
 if test -d "/Applications/SentinelOne/"; then
     log "It looks like SentinelOne is already installed!"
-    exit
+    exit 0 
 else
     log "SentinelOne is not detected. Begin installation."
     logNoTime ""
-
+    
     # Create token file
     log "Creating Token"
-    echo $regToken >$myDir"/com.sentinelone.registration-token"
+    echo "$regToken" >"$myDir/com.sentinelone.registration-token"
     logNoTime ""
 
     # Make sure it's readable
@@ -138,40 +138,49 @@ else
     # Download Installer
     # (The '&& log' ensures the download is done before proceeding)
     log "Begin Download"
-    curl -L -o $myDir"/S1.pkg" $fileLoc && log "Download complete!"
+    if curl -L -o "$myDir/S1.pkg" "$fileLoc"; then
+        log "Download complete"
+    else
+        log "Download error."
+        exit 1
+    fi
+
     logNoTime ""
 
     # Wait a few seconds... just in case!
     log "Waiting 10s"
     sleep 10
     logNoTime ""
-
+    
     # Run the installer
     log "Beginning Installation"
     sudo installer -pkg $myDir"/S1.pkg" -target /
     logNoTime ""
-
+    
     # Check installation
     if test -d "/Applications/SentinelOne/"; then
         logNoTime ""
-        logFancy "Successful installation!\nConfirm in the SentinelOne Portal\n\nEndpoint name: $HOSTNAME"
+        logNoTime "Successful installation!"
+        logNoTime "Confirm in the SentinelOne Portal"
+        logNoTime " "
+        logNoTime "Endpoint name: $HOSTNAME"
     else
         logNoTime ""
         logNoTime "Something went wrong =["
         logNoTime ""
     fi
-
+    
     # Clean up after yourself!
     # But don't delete the log file
     rm $myDir"/S1.pkg"
     rm $myDir"/com.sentinelone.registration-token"
-
+    
 fi
 
 logNoTime ""
 log "You've made it to the end, traveller!"
 
-exit
+exit 0
 
 # Done
 ###################################################################
